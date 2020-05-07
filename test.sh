4c4f57885ed04f42d80e4c006638bf22cd8aa617
NewDividedc <58285795+NewDividedc@users.noreply.github.com>
2020-05-07 15:06:26 +0800

    Delete test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-for file in chap0x04/code/*.sh;do
-	if [[ $file =~ $0 ]];then
-		continue
-	fi
-	printf "============== %s ====================\n" "$file"
-	bash "$file"
-done
